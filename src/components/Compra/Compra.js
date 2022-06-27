98fed326efe2248da003f65fea18bf44c5a447de
DiegoBandhy <diegoby1994@gmail.com>
2022-06-26 23:28:12 -0500

    Id corregido y todas las paginas abren. Context por desarrollar

--- src/components/Compra/Compra.js
+++ src/components/Compra/Compra.js
@@ -1,10 +1,15 @@
 // import { useState, useEffect } from "react"
 // import { getProductos , getProductoByCategoria } from "../../asyncmock"
 // import { useParams } from "react-router-dom"
+import Seleccionar from "../Seleccionar/Seleccionar"
 
 const Compra = () => {
     return (
-        <h1>    COMPRA </h1>
+        <article>    
+            <h1>    COMPRA </h1>
+                <h2> Seleccione la opción de compra </h2>
+                        <Seleccionar/> 
+        </article>        
     )
     
 }
--- src/components/Seleccionar/Seleccionar.js
+++ src/components/Seleccionar/Seleccionar.js
@@ -1,25 +1,72 @@
-function Seleccionar ({ opciones, onSelect, defaultOpcion}) {
-    return opciones.map (o => (<>
-    <input onChange={(e) => {
-        onSelect(o.value)
-    }}
-    type="radio"
-    name="endulzante"
-    checked={defaultOpcion === o.value}
-    id={o.value}
-    />
+import React, { Component } from "react";
 
-    <label for={o.value}>{o.text}</label>
-    
-    </>));
-}
-
-export default function Seleccionar() {
-    const [opcion, setOpcion] = useState(1);
-    const opciones = [{ value: 1, text: "edulcorante"}, { value: 2, text: "azúcar"}];
-
-    function opcionSeleccionada(value) {
-       setOpcion(value) 
+class Seleccionar extends Component {
+    recojo() {
+        alert("Te enviaremos la dirección a tu correo electrónico");
     }
+
+    envio() {
+        alert("Mándanos tu dirección por correo electrónico");
+    }
+
+    render() {
+        return (
+            <div onClick={this.Seleccionar}>
+                <button onClick={this.recojo}>Recoger</button>
+                <button onClick={this.envio}>Delivery</button>
+               
+            </div>
+        );
+    }
+
 }
+
+export default Seleccionar;
+
+
+
+// function Seleccionar() {
+//     function handleSubmit(e) {
+//       e.preventDefault();
+//       console.log('You clicked submit.');
+//     }
+  
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         <button type="submit">Escoge la forma de envío</button>
+//       </form>
+//     );
+//   }
+
+// export default Seleccionar
+
+
+
+
+// import { useState } from "react"
+ 
+// function Seleccionar ({ opciones, onSelect, defaultOpcion}) {
+//     return opciones.map (o => (<>
+//     <input onChange={(e) => {
+//         onSelect(o.value)
+//     }}
+//     type="radio"
+//     name="endulzante"
+//     checked={defaultOpcion === o.value}
+//     id={o.value}
+//     />
+
+//     <label for={o.value}>{o.text}</label>
+    
+//     </>));
+// }
+
+// export default Seleccionar() {
+//     const [opcion, setOpcion] = useState(1);
+//     const opciones = [{ value: 1, text: "edulcorante"}, { value: 2, text: "azúcar"}];
+
+//     function opcionSeleccionada(value) {
+//        setOpcion(value) 
+//     }
+// }
 
